690784817691ddf4a97ddb3f384c4e887c5a3f8c
Johndel B. Villanueva <vjohndel07@yahoo.com>
2018-07-12 20:02:29 +0800

    Initial Commit
    -10% World Map
    *Lighting and Animation
    *Obstacles and Tutorial
    *Story
    
    -First Person character (WIP):
    *Interact with objects
    *IDE UI

---
+++ Assets/Scripts/Player/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public string gameStatus;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		gameStatus = "playing";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(2))
+		{
+			if (gameStatus == "playing")
+			{
+				PauseGame();
+				Cursor.lockState = CursorLockMode.None;
+			}
+			else
+			{
+				ContinueGame();
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+		}
+	}
+
+	private void PauseGame()
+	{
+		Time.timeScale = 0;
+		gameStatus = "paused";
+	}
+
+	private void ContinueGame()
+	{
+		Time.timeScale = 1;
+		gameStatus = "playing";
+	}
+}
