50a86d678e950a83ac1cca3454a87bdc220ff673
Johndel <vjohndel07@yahoo.com>
2018-07-15 13:01:20 +0800

    Added Testing Scripts Folder

---
+++ Assets/Scenes/Test/Testing Scripts/resize.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class resize : MonoBehaviour {
+
+    [Header("GUI")]
+    public InputField xyz;
+    public InputField x;
+    public InputField y;
+    public InputField z;
+
+    private int xyzValue;
+    private int xValue;
+    private int yValue;
+    private int zValue;
+
+	// Update is called once per frame
+	void Update () {
+
+        try
+        {
+            xyzValue = Convert.ToInt32(xyz.text);
+            transform.localScale += new Vector3(xyzValue, xyzValue, xyzValue);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+        }
+	}
+}
