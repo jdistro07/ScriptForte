655b608ae4c7b0c62d810ac992c229ee46200908
Johndel <vjohndel07@yahoo.com>
2018-07-22 00:08:47 +0800

    Added SFX & Skybox Assets | Level Progress(Tutorial) 20%

---
+++ Assets/Scripts/Environment/platform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour {
+
+    public AudioSource stepSound;
+    public float pitch; // pitch to specific sfx relative to the platform count a player has stepped
+    public bool stepped; // check if platform is stepped
+
+}
