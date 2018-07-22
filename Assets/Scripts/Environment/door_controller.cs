dcb1e3948fa28d40ab04ef3b0c7e13b405afc178
Johndel <vjohndel07@yahoo.com>
2018-07-22 18:42:57 +0800

    Added gate animation with sound : not baked

---
+++ Assets/Scripts/Environment/door_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_controller : MonoBehaviour {
+
+    GameObject player;
+    public Transform gate;
+    public AudioSource sfx;
+
+    void OnTriggerEnter(Collider playerCollision)
+    {
+        var doorControl = gate.GetComponent<Animator>();
+
+        if(playerCollision.transform.name == "FPSController"){
+            doorControl.SetInteger("open", 1);
+            sfx.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider playerCollision2)
+    {
+        var doorControl = gate.GetComponent<Animator>();
+        if (playerCollision2.transform.name == "FPSController")
+        {
+            doorControl.SetInteger("open", 0);
+            sfx.Play();
+        }
+    }
+}
