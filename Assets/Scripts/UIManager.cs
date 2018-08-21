351839fbd8560f5265efa9b5cabe3593e3a96713
Johndel <vjohndel07@yahoo.com>
2018-08-21 15:10:26 +0800

    added UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	[Header("Main UI")]
+	public GameObject loginPanel;
+
+	[Header("Universal UI")]
+	public GameObject gameSettingsPanel;
+
+	public void OpengameSettings(){
+		loginPanel.active = false;
+		gameSettingsPanel.active = true;
+	}
+
+	public void ClosegameSettings(){
+		loginPanel.active = true;
+		gameSettingsPanel.active = false;
+	}
+	
+}
