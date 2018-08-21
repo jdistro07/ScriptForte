031f78ef1da2052d01a43476287eb65bfa44ef0c
Johndel <vjohndel07@yahoo.com>
2018-08-21 16:13:14 +0800

    Added UI sounds on Main

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,20 +5,47 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	[Header("Main UI")]
+	[Header("Main UI Component")]
 	public GameObject loginPanel;
 
-	[Header("Universal UI")]
+	[Header("Universal UI Component")]
 	public GameObject gameSettingsPanel;
 
+	[Header("Sound Effects")]
+	public AudioSource UIAudioSource;
+	public AudioClip openSfx;
+	public AudioClip closeSfx;
+	public AudioClip specialSfx;
+
+	private void Start()
+	{
+		
+	}
+
+	//universal methods
+	public void sfxOpen(){
+		UIAudioSource.PlayOneShot(openSfx);
+	}
+
+	public void sfxClose(){
+		UIAudioSource.PlayOneShot(closeSfx);
+	}
+
+	public void sfxSpecial(){
+		UIAudioSource.PlayOneShot(specialSfx);
+	}
+	//universal methods END
+
+	//universal methods
 	public void OpengameSettings(){
-		loginPanel.active = false;
-		gameSettingsPanel.active = true;
+		loginPanel.SetActive(false);
+		gameSettingsPanel.SetActive(true);
 	}
 
 	public void ClosegameSettings(){
-		loginPanel.active = true;
-		gameSettingsPanel.active = false;
+		loginPanel.SetActive(true);
+		gameSettingsPanel.SetActive(false);
 	}
+	//universal methods END
 	
 }
