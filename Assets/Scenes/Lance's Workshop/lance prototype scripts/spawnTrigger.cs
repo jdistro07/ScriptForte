6f5e52d5922d3572012489cadf7fcdb44415c01b
Jasper Lance Dayon <jasperlancecdayon@yahoo.com>
2018-08-24 22:39:02 +0800

    Quiz Mechanics almost complete  - end_platform offset fixed  - player is spawned at the beginning of the test  - player falling off the platform is now respawned

---
+++ Assets/Scenes/Lance's Workshop/lance prototype scripts/spawnTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnTrigger : MonoBehaviour
+{
+	[Header("Respawn Settings")]
+	[SerializeField] private bool respawnTrigger = false;
+	[SerializeField] private Transform spawnPoint;
+
+	[Header("Gate Trigger Settings")]
+	[SerializeField] private string answerValue;
+	public static string answer;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			//normal gate trigger
+			answer = answerValue;
+			//close gate
+
+			//player fall respawn trigger
+			if (respawnTrigger == true)
+			{
+				Transform player = GameObject.FindWithTag ("Player").GetComponent<Transform> ();
+				player.transform.position = spawnPoint.position;
+			}
+		}
+	}
+}
