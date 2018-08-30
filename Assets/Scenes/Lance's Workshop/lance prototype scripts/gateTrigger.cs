c33b5f786188cfa680833b3cc7609fd849067cc8
Johndel <vjohndel07@yahoo.com>
2018-08-30 20:03:13 +0800

    Added close animation. Trigger for open needs to be destroyed when sector is constructed

--- Assets/Scenes/Lance's Workshop/lance prototype scripts/gateTrigger.cs
+++ Assets/Scenes/Lance's Workshop/lance prototype scripts/gateTrigger.cs
@@ -6,17 +6,18 @@ public class gateTrigger : MonoBehaviour
 {
 	[SerializeField] private Animator gateAnim;
 	[SerializeField] private GameObject platformSpawn;
-
+	
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.transform.tag == "Player")
 		{
 			gateAnim.enabled = true;
+			gateAnim.SetInteger("gateOpen", 1);
 		}
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		//close gate
+		gateAnim.SetInteger("gateOpen", 2);
 	}
 }
