b925c5874795febb044b193246d25f92f9da7f30
Johndel <vjohndel07@yahoo.com>
2018-09-04 19:33:36 +0800

    Added SFX and random shoot time

--- Assets/Scenes/Lance's Workshop/lance prototype scripts/AIBehaviour.cs
+++ Assets/Scenes/Lance's Workshop/lance prototype scripts/AIBehaviour.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class AIBehaviour : MonoBehaviour
 {
+	[SerializeField] AudioClip shootSound;
+
 	public Transform target;
 	public GameObject projectile;
 	public float projectileSpawnOffset;
 	public float projectileStrength;
+
+	[SerializeField,Range(2,5)]
 	public float shootInterval;
+
 	public int moveSpeed;
 	public float hoverHeight;
 	public float hoverStrength;
@@ -80,9 +85,13 @@ public class AIBehaviour : MonoBehaviour
 	IEnumerator shoot()
 	{
 		Transform bulletSpawn = transform.Find ("bulletSpawn");
+		AudioSource sfx_shoot = bulletSpawn.GetComponent<AudioSource>();
+
+		sfx_shoot.PlayOneShot(shootSound);
 		bullet = Instantiate (projectile, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
 
 		fire = false;
+		shootInterval = Random.Range(2,5);
 		yield return new WaitForSeconds (shootInterval);
 		fire = true;
 	}
