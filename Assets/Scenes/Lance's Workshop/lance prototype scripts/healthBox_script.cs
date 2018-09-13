8ab20933eef3006f96e47e2b68a5cc7859abd51e
Jasper Lance C. Dayon <jasperlancecdayon@yahoo.com>
2018-09-13 16:33:50 +0000

    Merged in devLance (pull request #10)
    testHandler now works with DBProcessor
    
    Additions:
    1. HP object that adds 1 HP to the player
    2. User credentials and overall consistency of the user that reflects their performance throughout the learning process is now applied to  the profile panel.
    
    Major Fixes:
    1. Platform fall HP reduction
    2. Canvas UI no longer spawns at the first platform

---
+++ Assets/Scenes/Lance's Workshop/lance prototype scripts/healthBox_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class healthBox_script : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		var FPC = player.GetComponent<FirstPersonController> ();
+
+
+		if (other.transform.tag == "Player")
+		{
+			if (FPC.playerLife < FPC.maxHealth)
+			{
+				FPC.playerLife += 1;
+				Destroy (gameObject);
+			}
+		}
+	}
+}
