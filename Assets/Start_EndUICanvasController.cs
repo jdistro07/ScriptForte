4bcaa11fc05d72aa4d726a9a880ea677bd7961b4
Johndel <vjohndel07@yahoo.com>
2018-09-19 01:10:25 +0800

    Added medical box

--- Assets/Start_EndUICanvasController.cs
+++ Assets/Start_EndUICanvasController.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Start_EndUICanvasController : MonoBehaviour {
 
-	[SerializeField]DBContentProcessor dbContent_Processor;
-	[SerializeField]UIManager uiManager;
+	[SerializeField] DBContentProcessor dbContent_Processor;
+	[SerializeField] UIManager uiManager;
 
 	[SerializeField] Button btnFinish;
 
 
 	// Use this for initialization
-	private void Start()
+	private void Awake()
 	{
 		
 		dbContent_Processor = GameObject.Find("AIOGameManager").GetComponent<DBContentProcessor>();
 		uiManager = GameObject.Find("AIOGameManager").GetComponent<UIManager>();
 
 		btnFinish.onClick.AddListener(() => {
+
+			Debug.Log("Leaving world");
 
 			uiManager.sfxSpecial();
 			dbContent_Processor.OnClickInGameFinish();
