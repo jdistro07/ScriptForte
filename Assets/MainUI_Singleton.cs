e99c023337e2b41f4edd55d0dcc759059f883801
Johndel <vjohndel07@yahoo.com>
2018-09-21 00:17:11 +0800

    - Proper singleton for MainUI canvas and is only visible to Main UI scene - Clean up of test items on disable of the canvas to avoid duplications when enabled

---
+++ Assets/MainUI_Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUI_Singleton : MonoBehaviour {
+
+	static MainUI_Singleton instance;
+
+	private void Awake()
+	{
+
+		if(instance != null){
+
+			Destroy(gameObject);
+
+		}else{
+
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+
+		}
+
+		
+
+	}
+
+}
---
+++ Assets/SingletonSceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SingletonSceneController : MonoBehaviour {
+
+	[SerializeField] GameObject mainUI;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		if(SceneManager.GetActiveScene().name == "Main UI"){
+
+			mainUI.SetActive(true);
+
+		}else{
+
+			StartCoroutine(itemCleanUp());
+			mainUI.SetActive(false);
+		
+		}
+
+	}
+
+	IEnumerator itemCleanUp(){
+
+		var itemPanels = GameObject.FindGameObjectsWithTag("List Item");
+		
+		foreach(GameObject items in itemPanels){
+
+			Destroy(items);
+			yield return null;
+
+		}
+
+	}
+}
