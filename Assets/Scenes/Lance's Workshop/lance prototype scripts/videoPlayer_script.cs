a92ce66113719c5614b007dea8fd70b772910c34
Jasper Lance Dayon <jasperlancecdayon@yahoo.com>
2018-09-22 00:47:53 +0800

    Video Controlls Added:  - Play  - Pause  - Stop  - Progress Bar

--- Assets/Scenes/Lance's Workshop/lance prototype scripts/videoPlayer_script.cs
+++ Assets/Scenes/Lance's Workshop/lance prototype scripts/videoPlayer_script.cs
@@ -8,16 +8,76 @@ public class videoPlayer_script : MonoBehaviour
 {
 	public string videoURL;
 
-	public RawImage rawImage;
-	public VideoPlayer videoPlayer;
-	public AudioSource audioSource;
+	[SerializeField] private RawImage rawImage;
+	[SerializeField] private VideoPlayer videoPlayer;
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private Button playButton;
+	[SerializeField] private Button stopButton;
+	[SerializeField] private Slider seekBar;
+
+	private float vidLength;
+	private bool isDone = false;
 
 	private void Start()
 	{
-		StartCoroutine (playVideo ());
+		playButton.onClick.AddListener (playVideo);
+		stopButton.onClick.AddListener (stopVideo);
+
+		//StartCoroutine (playVideo ());
 	}
 
-	private IEnumerator playVideo()
+	void Update()
+	{
+		if (videoPlayer.url != videoURL && isDone == false)
+		{
+			Debug.Log("Loading Video");
+			videoPlayer.url = videoURL;
+			videoPlayer.Prepare ();
+		}
+		isDone = true;
+
+		if (videoPlayer.isPrepared)
+		{
+			vidLength = (float)videoPlayer.frameCount / (float)videoPlayer.frameRate;
+			seekBar.minValue = 0;
+			seekBar.maxValue = vidLength;
+		}
+
+		seekBar.value = (float)videoPlayer.time;
+	}
+
+	void playVideo()
+	{
+		if (!videoPlayer.isPrepared)
+		{
+			Debug.Log ("Video is still preparing");
+		}
+		else
+		{
+			if (!videoPlayer.isPlaying)
+			{
+				rawImage.texture = videoPlayer.texture;
+				videoPlayer.Play ();
+				audioSource.Play ();
+			}
+			else if (videoPlayer.isPlaying)
+			{
+				rawImage.texture = videoPlayer.texture;
+				videoPlayer.Pause ();
+				audioSource.Pause ();
+			}
+		}
+	}
+
+	void stopVideo()
+	{
+		if (!videoPlayer.isPlaying)
+			return;
+		videoPlayer.Stop ();
+		videoPlayer.Prepare ();
+	}
+
+	/*private IEnumerator playVideo()
 	{
 		videoPlayer.Prepare ();
 		while (!videoPlayer.isPrepared)
@@ -28,5 +88,5 @@ public class videoPlayer_script : MonoBehaviour
 		rawImage.texture = videoPlayer.texture;
 		videoPlayer.Play ();
 		audioSource.Play ();
-	}
+	}*/
 }
