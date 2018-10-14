1ef60624830b2e743c3ae7d4639f61064b8a280d
Johndel <vjohndel07@yahoo.com>
2018-10-14 22:55:30 +0800

    Scene management compiling script on build

--- Assets/LearningController.cs
+++ Assets/LearningController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class LearningController : MonoBehaviour {
 
